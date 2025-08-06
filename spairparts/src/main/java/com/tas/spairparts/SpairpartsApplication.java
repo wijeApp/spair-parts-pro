d0fae881254b78b32b1c96fac14381751404e62b
unknown <wijemobapp@gmail.com>
2025-08-06 19:12:59 +0530

    spair-parts 01

---
+++ spairparts/src/main/java/com/tas/spairparts/SpairpartsApplication.java
@@ -0,0 +1,13 @@
+package com.tas.spairparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpairpartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpairpartsApplication.class, args);
+	}
+
+}
